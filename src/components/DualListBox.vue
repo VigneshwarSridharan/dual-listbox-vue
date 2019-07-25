eaefb223b9d93e8149b14f65dfe9477f7a060802
Vigneshwar <vigneshwar.s>
2019-07-25 19:42:23 +0530

    adding icon

--- src/components/DualListBox.vue
+++ src/components/DualListBox.vue
@@ -17,16 +17,36 @@
     </div>
     <div class="actions">
       <div class="btn-action" @click="moveDestination">
-        <img :src="angleRight" height="18" />
+        <svg height="18" viewBox="0 0 256 512">
+          <path
+            fill="#ffffff"
+            d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"
+          />
+        </svg>
       </div>
       <div class="btn-action" @click="moveAllDestination">
-        <img :src="angleDoubleRight" height="18" />
+        <svg height="18" viewBox="0 0 448 512">
+          <path
+            fill="#ffffff"
+            d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34zm192-34l-136-136c-9.4-9.4-24.6-9.4-33.9 0l-22.6 22.6c-9.4 9.4-9.4 24.6 0 33.9l96.4 96.4-96.4 96.4c-9.4 9.4-9.4 24.6 0 33.9l22.6 22.6c9.4 9.4 24.6 9.4 33.9 0l136-136c9.4-9.2 9.4-24.4 0-33.8z"
+          />
+        </svg>
       </div>
       <div class="btn-action" @click="moveSource">
-        <img :src="angleLeft" height="18" />
+        <svg height="18" viewBox="0 0 256 512">
+          <path
+            fill="#ffffff"
+            d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"
+          />
+        </svg>
       </div>
       <div class="btn-action" @click="moveAllSource">
-        <img :src="angleDoubleLeft" height="18" />
+        <svg height="18" viewBox="0 0 448 512">
+          <path
+            fill="#ffffff"
+            d="M223.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L319.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L393.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34zm-192 34l136 136c9.4 9.4 24.6 9.4 33.9 0l22.6-22.6c9.4-9.4 9.4-24.6 0-33.9L127.9 256l96.4-96.4c9.4-9.4 9.4-24.6 0-33.9L201.7 103c-9.4-9.4-24.6-9.4-33.9 0l-136 136c-9.5 9.4-9.5 24.6-.1 34z"
+          />
+        </svg>
       </div>
     </div>
     <div>
@@ -258,7 +278,7 @@ export default {
   border-color: #007bff;
   cursor: pointer;
 }
-.btn-action img {
+.btn-action svg {
   vertical-align: middle;
 }
 
